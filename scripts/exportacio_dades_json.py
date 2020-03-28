b5aade16a5970d10ed011471fbce0756a40bfe09
Antonio Morillas <amorilla@xtec.cat>
2020-03-28 16:03:29 +0100

    Actualitza fixtures

---
+++ scripts/exportacio_dades_json.py
@@ -0,0 +1,32 @@
+from django.core.serializers import serialize
+from aula.apps.assignatures.models import TipusDAssignatura
+from aula.apps.extEsfera.models import ParametreEsfera
+from aula.apps.extSaga.models import ParametreSaga
+from aula.apps.extKronowin.models import ParametreKronowin
+from aula.apps.incidencies.models import FrassesIncidenciaAula, TipusSancio, TipusIncidencia
+from aula.apps.presencia.models import EstatControlAssistencia
+from django.contrib.auth.models import Group
+
+def exportJson(dades, app):
+    f= open("aula/apps/"+app+"/fixtures/dades.json","w+")
+    f.write(dades)
+    f.close() 
+
+dades=serialize('json', TipusDAssignatura.objects.all())
+exportJson(dades,'assignatures')
+dades=serialize('json', ParametreEsfera.objects.all())
+exportJson(dades,'extEsfera')
+dades=serialize('json', ParametreSaga.objects.all())
+exportJson(dades,'extSaga')
+dades=serialize('json', ParametreKronowin.objects.all())
+exportJson(dades,'extKronowin')
+dades=serialize('json', FrassesIncidenciaAula.objects.all())
+dades=dades[:len(dades)-1]+","+serialize('json', TipusSancio.objects.all())[1:]
+dades=dades[:len(dades)-1]+","+serialize('json', TipusIncidencia.objects.all())[1:]
+exportJson(dades,'incidencies')
+dades=serialize('json', EstatControlAssistencia.objects.all())
+exportJson(dades,'presencia')
+dades=serialize('json', Group.objects.all())
+exportJson(dades,'usuaris')
+
+quit()
