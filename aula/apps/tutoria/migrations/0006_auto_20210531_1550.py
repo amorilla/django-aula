7177b75d594d496f3cc0e0a475948beb4ef8a76a
Antonio Morillas <amorilla@xtec.cat>
2022-08-05 00:28:08 +0200

    Canvi especial
    Per compatibilitat amb dades antigues

--- aula/apps/tutoria/migrations/0006_auto_20210531_1550.py
+++ aula/apps/tutoria/migrations/0006_auto_20210531_1550.py
@@ -13,6 +13,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='actuacio',
             name='assumpte',
-            field=models.CharField(choices=[('T', 'Tutoria individualitzada'), ('C', 'A/I Conflicte comportament'), ('V', 'A/I Valoració'), ('S', 'A/I Seguiment'), ('O', 'A/I Orientació acadèmica'), ('E', 'A/I Suport educatiu'), ('G', 'A/I Gestió de les emocions'), ('I', 'A/I Gestió social'), ('P', 'A/I Pla individualitzat'), ('A', 'Actuació puntual')], help_text='Assumpte', max_length=256),
+            field=models.CharField(choices=[('T', 'Tutoria individualitzada'), ('C', 'A/I Conflicte comportament'), ('V', 'A/I Valoració'), ('S', 'A/I Seguiment'), ('O', 'A/I Orientació acadèmica'), ('E', 'A/I Suport educatiu'), ('G', 'A/I Gestió de les emocions'), ('I', 'A/I Gestió social'), ('P', 'A/I Pla individualitzat'), ('A', 'Actuació puntual')], help_text='Assumpte', max_length=200),
         ),
     ]
